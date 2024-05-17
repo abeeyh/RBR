75c3f97380d3bc4bb6eff0e30cba1f64a21f841f
abeey <messina@abeeyh.com>
2024-05-17 05:40:17 -0300

    fix: add cors for localhost:3001

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const mongoose = require('mongoose');
-const employeeRoutes = require('./components/routes/employeeRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const employeeRoutes = require("./components/routes/employeeRoutes");
 
 const app = express();
-app.use(bodyParser.json());
-app.use('/api/employees', employeeRoutes);
 
-mongoose.connect('mongodb://localhost:27017/employeeDB', {
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
+
+app.use(bodyParser.json());
+app.use("/api/employees", employeeRoutes);
+
+mongoose
+  .connect("mongodb://localhost:27017/employeeDB", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Conectado ao MongoDB"))
-.catch(err => console.error("Não foi possível conectar ao MongoDB", err));
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.error("Não foi possível conectar ao MongoDB", err));
 
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API de Funcionários',
-            version: '1.0.0',
-            description: 'API para gestão de funcionários',
-        },
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de Funcionários",
+      version: "1.0.0",
+      description: "API para gestão de funcionários",
     },
-    apis: ['./components/routes/*.js', './components/models/*.js'],
+  },
+  apis: ["./components/routes/*.js", "./components/models/*.js"],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
