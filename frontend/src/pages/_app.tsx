a6a59418969567310a77cad78b1140528077d92b
abeey <messina@abeeyh.com>
2024-05-17 05:45:44 -0300

    fix: add styles for chackra errormessage

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { SnackbarProvider } from '@/utils/context/SnackBar/SnackBar';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
