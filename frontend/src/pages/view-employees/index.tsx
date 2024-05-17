b84b10318a219cdb2480dda2b6fa0a47574f870c
abeey <messina@abeeyh.com>
2024-05-17 06:03:44 -0300

    feat: add button new employee on view table

--- frontend/src/pages/view-employees/index.tsx
+++ frontend/src/pages/view-employees/index.tsx
@@ -1,93 +1,110 @@
 import {
-    Box,
-    Container,
-    Heading,
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    Button,
-    Input,
-    Select,
-    Stack,
-  } from "@chakra-ui/react";
-  import { useState, useEffect, Key } from "react";
-  import NavBar from "@/components/molecules/navbar/NavBar";
-  import { Position } from "@/utils/enum/Position";
-  import { Department } from "@/utils/enum/Department";
-  import Employee from "@/Types/Employee/Employee";
-  import { useRouter } from 'next/router';
+  Box,
+  Container,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  Button,
+  Input,
+  Select,
+  Stack,
+  Flex,
+} from "@chakra-ui/react";
+import { useState, useEffect, Key } from "react";
+import NavBar from "@/components/molecules/navbar/NavBar";
+import { Position } from "@/utils/enum/Position";
+import { Department } from "@/utils/enum/Department";
+import Employee from "@/Types/Employee/Employee";
+import { useRouter } from "next/router";
 import { deleteEmployee, getEmployees } from "@/utils/services/EmployeeService";
 import { formatDate } from "@/utils/dateFormatter/dateFormater";
-  
-  const ViewEmployees = () => {
-    const router = useRouter();
-    const [employees, setEmployees] = useState<Employee[]>([]);
-    const [filters, setFilters] = useState({
-      name: "",
-      position: "",
-      department: "",
-      dateOfHire: "",
-    });
-  
-    useEffect(() => {
+
+const ViewEmployees = () => {
+  const router = useRouter();
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const [filters, setFilters] = useState({
+    name: "",
+    position: "",
+    department: "",
+    dateOfHire: "",
+  });
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const fetchEmployees = async () => {
+    try {
+      const data = await getEmployees();
+      setEmployees(data);
+    } catch (error) {
+      console.error("Failed to fetch employees", error);
+    }
+  };
+
+  const handleFilterChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
+  };
+
+  const handleDelete = async (id: Key) => {
+    try {
+      await deleteEmployee(id);
       fetchEmployees();
-    }, []);
-  
-    const fetchEmployees = async () => {
-      try {
-        const data = await getEmployees();
-        setEmployees(data);
-      } catch (error) {
-        console.error("Failed to fetch employees", error);
-      }
-    };
-  
-    const handleFilterChange = (
-      e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-    ) => {
-      const { name, value } = e.target;
-      setFilters((prevFilters) => ({
-        ...prevFilters,
-        [name]: value,
-      }));
-    };
-  
-    const handleDelete = async (id: Key) => {
-      try {
-        await deleteEmployee(id);
-        fetchEmployees();
-      } catch (error) {
-        console.error(`Failed to delete employee with ID: ${id}`, error);
-      }
-    };
-  
-    const handleEdit = (id: Key) => {
-      router.push(`/edit-employee?id=${id}`);
-    };
-  
-    const filteredEmployees = employees.filter((employee) => {
-      return (
-        (filters.name === "" ||
-          employee.name.toLowerCase().includes(filters.name.toLowerCase())) &&
-        (filters.position === "" || employee.position === filters.position) &&
-        (filters.department === "" ||
-          employee.department === filters.department) &&
-        (filters.dateOfHire === "" || employee.dateOfHire === filters.dateOfHire)
-      );
-    });
-  
+    } catch (error) {
+      console.error(`Failed to delete employee with ID: ${id}`, error);
+    }
+  };
+
+  const handleEdit = (id: Key) => {
+    router.push(`/edit-employee?id=${id}`);
+  };
+
+  const handleAddNew = () => {
+    router.push("/register-employee");
+  };
+
+  const filteredEmployees = employees.filter((employee) => {
     return (
-      <Box>
-        <NavBar />
-        <Container maxW="container.xl" py={10}>
-          <Heading as="h2" size="xl" mb={6}>
-            Visualização de Funcionários
-          </Heading>
-          <Stack direction="row" spacing={4} mb={6}>
+      (filters.name === "" ||
+        employee.name.toLowerCase().includes(filters.name.toLowerCase())) &&
+      (filters.position === "" || employee.position === filters.position) &&
+      (filters.department === "" ||
+        employee.department === filters.department) &&
+      (filters.dateOfHire === "" || employee.dateOfHire === filters.dateOfHire)
+    );
+  });
+
+  return (
+    <Box>
+      <NavBar />
+      <Container maxW="container.xl" py={10}>
+        <Heading as="h2" size="xl" mb={6}>
+          Visualização de Funcionários
+        </Heading>
+        <Flex
+          justify="space-between"
+          align="center"
+          justifyContent="flex-end"
+          mb={6}
+        >
+          <Button colorScheme="green" onClick={handleAddNew}>
+            Adicionar Novo Funcionário
+          </Button>
+        </Flex>
+        <Flex justify="space-between" align="center" mb={6}>
+          Filtros:
+          <Stack direction="row" spacing={6}>
             <Input
               placeholder="Nome"
               name="name"
@@ -126,51 +143,51 @@ import { formatDate } from "@/utils/dateFormatter/dateFormater";
               onChange={handleFilterChange}
             />
           </Stack>
-          <Box overflowX="auto">
-            <Table variant="striped">
-              <TableCaption>Lista de Funcionários</TableCaption>
-              <Thead>
-                <Tr>
-                  <Th>Nome</Th>
-                  <Th>Cargo</Th>
-                  <Th>Departamento</Th>
-                  <Th>Data de Admissão</Th>
-                  <Th>Ações</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {filteredEmployees.map((employee) => (
-                  <Tr key={employee._id}>
-                    <Td>{employee.name}</Td>
-                    <Td>{employee.position}</Td>
-                    <Td>{employee.department}</Td>
+        </Flex>
+        <Box overflowX="auto">
+          <Table variant="striped">
+            <TableCaption>Lista de Funcionários</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Nome</Th>
+                <Th>Cargo</Th>
+                <Th>Departamento</Th>
+                <Th>Data de Admissão</Th>
+                <Th>Ações</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {filteredEmployees.map((employee) => (
+                <Tr key={employee._id}>
+                  <Td>{employee.name}</Td>
+                  <Td>{employee.position}</Td>
+                  <Td>{employee.department}</Td>
                   <Td>{formatDate(employee.dateOfHire)}</Td>
-                    <Td>
-                      <Button
-                        colorScheme="blue"
-                        size="sm"
-                        mr={2}
-                        onClick={() => handleEdit(employee._id!)}
-                      >
-                        Editar
-                      </Button>
-                      <Button
-                        colorScheme="red"
-                        size="sm"
-                        onClick={() => handleDelete(employee._id!)}
-                      >
-                        Excluir
-                      </Button>
-                    </Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </Box>
-        </Container>
-      </Box>
-    );
-  };
-  
-  export default ViewEmployees;
-  
+                  <Td>
+                    <Button
+                      colorScheme="blue"
+                      size="sm"
+                      mr={2}
+                      onClick={() => handleEdit(employee._id!)}
+                    >
+                      Editar
+                    </Button>
+                    <Button
+                      colorScheme="red"
+                      size="sm"
+                      onClick={() => handleDelete(employee._id!)}
+                    >
+                      Excluir
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default ViewEmployees;
